4110eaad03ccd4fc33a070ff651ebaea31894896
DaniaG1 <daniagomez@umariana.edu.co>
2021-10-13 21:37:25 -0500

    Nivel 1
    Se actualizo el nivel 1 del videojuego

---
+++ ASTRA_17/Assets/Scripts/MovCam.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovCam : MonoBehaviour
+{
+    public GameObject Heroe;
+
+    //La camara se horixontalmente con el Heroe.
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.x = Heroe.transform.position.x;
+        transform.position = position;
+    }
+}
